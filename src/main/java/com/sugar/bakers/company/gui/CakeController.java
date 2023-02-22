f85687a8f32e9de8b7c76ac4872c4a63c8a5da7a
marcel <marcel.zebrowski@gmx.de>
2023-02-22 12:55:46 +0100

    Save the current state

--- src/main/java/com/sugar/bakers/company/gui/CakeController.java
+++ src/main/java/com/sugar/bakers/company/gui/CakeController.java
@@ -1,6 +1,7 @@
 package com.sugar.bakers.company.gui;
 
-import com.sugar.bakers.company.domain.Order;
+import com.sugar.bakers.company.domain.Cake;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,13 @@ import java.util.List;
 @Controller
 public class CakeController {
 
-    public class Cake{
+    @Value("{$bakershexagon.service.url}")
+    private String orderRestControllerUrl;
+
+    @Value("{$bakershexagon.service.port}")
+    private String orderRestControllerPort;
+
+    /*public class Cake{
         public String name;
         public String beschreibung;
 
@@ -20,20 +27,19 @@ public class CakeController {
             this.name = name;
             this.beschreibung = beschreibung;
         }
-    }
+    }*/
 
 
     @RequestMapping(value = {"/cake","/"}, method = RequestMethod.GET)
     public String showAllCakes(Model model){
 
         List<Cake> cakeList = new ArrayList<>();
-        cakeList.add(new Cake("Test Kuchen", "Ganz toller Kuchen"));
-        cakeList.add(new Cake("Test Kuchen2", "Ganz toller Kuchen2"));
+        cakeList.add(new Cake("Test Kuchen", "Ganz toller Kuchen" ,"dd"));
+        cakeList.add(new Cake("Test Kuchen2", "Ganz toller Kuchen2", ""));
 
         model.addAttribute("cakes",cakeList);
 
 
-        Order order;
 
         return "cake";
     }
